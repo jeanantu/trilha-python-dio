3e04df23630e161b1734e580c5afc04640f699e6
jeanantu <jeanantu@yahoo.com.br>
2025-01-21 14:20:33 -0300

    commit tuplas

--- 01 - Estrutura de dados/02 - Tuplas/00_declarando_tuplas.py
+++ 01 - Estrutura de dados/02 - Tuplas/00_declarando_tuplas.py
@@ -2,7 +2,7 @@ frutas = (
     "laranja",
     "pera",
     "uva",
-)
+)                ### diferença entre tupla e lista eh que tuplas nao podem ser modificadas. Esse eh objetivo tupla a garantia que nao sera modificada
 print(frutas)
 
 letras = tuple("python")
@@ -11,5 +11,5 @@ print(letras)
 numeros = tuple([1, 2, 3, 4])
 print(numeros)
 
-pais = ("Brasil",)
+pais = ("Brasil",)   ### em tupla se coloca "," ao final para o interpretador ter a certeza que se trata de uma dupla
 print(pais)
